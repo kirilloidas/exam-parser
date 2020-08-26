944340e2b0f9ed4e68d81e65ecb15a7150e20f4a
kirilloidas <peregudakirill90@gmail.com>
2020-08-27 01:29:38 +0300

    Search v.2.0

--- script/Search.js
+++ script/Search.js
@@ -8,9 +8,21 @@ export class Search {
 
     static setCityCoordinates(city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b352df7456cbd31cf07f70660e3eb572`)
-        .then(response => response.json())
+        .then(response => {
+            if(response.ok) {
+                return response.json()
+            } else {
+                document.location.href = './404-error.html';
+            }
+            
+        })
         .then(data => {
-            Initialization.initializationAjax(data.coord.lat, data.coord.lon);
-        });
+                Initialization.initializationAjax(data.coord.lat, data.coord.lon);
+        })
+        // .catch(
+        //     document.location.href = './404-error.html'
+        // );
+        
+        
     }
 }
