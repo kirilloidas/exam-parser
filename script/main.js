64a34c6f46cab3e2ee3722b5c16e6ba355033d8f
kirilloidas <peregudakirill90@gmail.com>
2020-08-28 19:02:29 +0300

    2

--- script/main.js
+++ script/main.js
@@ -5,15 +5,19 @@ import { Search } from './Search.js';
 import { NearbyPlaces } from './NearbyPlaces.js';
 
 window.addEventListener('load', () => {
-
+    
     document.addEventListener('keydown', function(event) {
         if(event.code == 'Enter') {
             Search.setCityCoordinates(header_search.value);
             insert();
         }
     });
+
+    
     
     function insert() {
+        DaysWeather.setDateUTC();
+        console.log(DaysWeather.getDateUTC());
         if(document.title == 'Weater-today') {
             setTimeout(() => {
                     current_weather_icon.src = CurrentWeather.getCurrentIcon();
@@ -31,16 +35,63 @@ window.addEventListener('load', () => {
                         city_temp[i].innerHTML = NearbyPlaces.getWeatherPlaces()[i].temp;
                     }
             
-                    for(let i = 0; i < hourly_icons.length; i++) {
-                        hourly_icons[i].src = HourlyWeather.getIcons()[i];
-                        hourly_description[i].innerHTML = HourlyWeather.getDescriptions()[i];
-                        hourly_temperature[i].innerHTML = HourlyWeather.getTemperatures()[i];
-                        hourly_temperature_feel[i].innerHTML = HourlyWeather.getTemperaturesFeel()[i];
-                        hourly_wind[i].innerHTML = `${Wind.getWindSpeed()[i]} ${Wind.getWindDirection()[i]}`;
-                    }
-    
-            }, 1000);
+                    Initialization.initializationHourlyDays(0);
+                    setTimeout(() => {
+                        for(let i = 0; i < DailyWeather.getHourlyWeather().length; i++) {
+                            if(DailyWeather.getHourlyWeather().length == hourly_icons.length) {
+                                break;
+                            }
+                            hourly_date[i].innerHTML = DailyWeather.getHourlyWeather()[i].dt;
+                            hourly_icons[i].src = DailyWeather.getHourlyWeather()[i].icon;
+                            hourly_description[i].innerHTML = DailyWeather.getHourlyWeather()[i].description;
+                            hourly_temperature[i].innerHTML = DailyWeather.getHourlyWeather()[i].temp;
+                            hourly_temperature_feel[i].innerHTML = DailyWeather.getHourlyWeather()[i].tempFeel;
+                            hourly_wind[i].innerHTML = DailyWeather.getHourlyWeather()[i].wind;
+                        }
+                    }, 1200);
+                    
+            }, 1200);
+            
         } else if (document.title == '5-day-forecast') {
+
+            forecast_list.addEventListener('click', function(event) {
+                if(event.target.tagName == 'LI') {
+                    Initialization.initializationHourlyDays(event.target.value);
+                    setTimeout(() => {
+                        for(let i = 0; i < DailyWeather.getHourlyWeather().length; i++) {
+                            if(DailyWeather.getHourlyWeather().length == hourly_icons.length) {
+                                break;
+                            }
+                            hourly_date[i].innerHTML = DailyWeather.getHourlyWeather()[i].dt;
+                            hourly_icons[i].src = DailyWeather.getHourlyWeather()[i].icon;
+                            hourly_description[i].innerHTML = DailyWeather.getHourlyWeather()[i].description;
+                            hourly_temperature[i].innerHTML = DailyWeather.getHourlyWeather()[i].temp;
+                            hourly_temperature_feel[i].innerHTML = DailyWeather.getHourlyWeather()[i].tempFeel;
+                            hourly_wind[i].innerHTML = DailyWeather.getHourlyWeather()[i].wind;
+                        }
+                        console.log(DailyWeather.getHourlyWeather());
+                    }, 2000);
+                    console.log(event.target.value);
+                } else {
+                    Initialization.initializationHourlyDays(event.target.parentNode.value);
+                    setTimeout(() => {
+                        for(let i = 0; i < DailyWeather.getHourlyWeather().length; i++) {
+                            if(DailyWeather.getHourlyWeather().length == hourly_icons.length) {
+                                break;
+                            }
+                            hourly_date[i].innerHTML = DailyWeather.getHourlyWeather()[i].dt;
+                            hourly_icons[i].src = DailyWeather.getHourlyWeather()[i].icon;
+                            hourly_description[i].innerHTML = DailyWeather.getHourlyWeather()[i].description;
+                            hourly_temperature[i].innerHTML = DailyWeather.getHourlyWeather()[i].temp;
+                            hourly_temperature_feel[i].innerHTML = DailyWeather.getHourlyWeather()[i].tempFeel;
+                            hourly_wind[i].innerHTML = DailyWeather.getHourlyWeather()[i].wind;
+                        };
+                    }, 2000);
+                    console.log(event.target.parentNode.value);
+                }
+                
+            });
+
             setTimeout(() => {
                 for(let i = 0; i < 5; i++) {
                     item_title[i].innerHTML = DaysWeather.getDayOfWeek()[i];
@@ -49,7 +100,7 @@ window.addEventListener('load', () => {
                     item_degrees[i].innerHTML = DailyWeather.getDailyTemp()[i];
                     item_precipitation[i].innerHTML = DailyWeather.getDailyDescription()[i];
                 }
-            }, 1000);
+            }, 2000);
         }
     }
     
